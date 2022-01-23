301bd72bd880049efbbfc2731fbac0d833f689f0
dansalart <kikesalas@gmail.com>
2022-01-22 21:54:17 -0300

    segundocommitDS

--- Blog/AppBlog/urls.py
+++ Blog/AppBlog/urls.py
@@ -7,7 +7,7 @@ from .views import inicio, galeria, artista, clientes
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('galeria/', galeria, name='galeria'),
-    path('artista/', artista, name='artistas'),
+    path('artista/', artista, name='artista'),
     path('clientes/', clientes, name='clientes'),
     # path('entregables', views.entregables, name = "entregables"),
 ]
