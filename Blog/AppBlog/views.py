9081cf56b309337107a4482544721c85e46b901f
dansalart <kikesalas@gmail.com>
2022-01-22 21:16:41 -0300

    commitds

--- Blog/AppBlog/views.py
+++ Blog/AppBlog/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.template import loader
 
+
 def inicio(request):
-    return render(request, "AppBlog/inicio.html")
+    return render(request, "AppBlog/inicio.html",{})
 
 def galeria(request):
-    return render(request, "AppBlog/galeria.html")
+    return render(request, "AppBlog/galeria.html",{})
 
 def clientes(request):
-    return render(request, "AppBlog/clientes.html")
+    return render(request, "AppBlog/clientes.html",{})
 
 def artista(request):
-    return render(request, "AppBlog/artista.html")
+    return render(request, "AppBlog/artista.html",{})
